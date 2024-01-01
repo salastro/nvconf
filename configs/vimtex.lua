021f3b35712a1d3cdd17569ce7f3922091d264ad
salastro <salah2112004@gmail.com>
2024-01-01 18:38:04 +0200

    LaTeX advanced surround

--- configs/vimtex.lua
+++ configs/vimtex.lua
@@ -49,3 +49,20 @@ function _G.dragPDF()
   -- Normal mode
   vim.cmd "stopinsert"
 end
+
+require("nvim-surround").buffer_setup {
+  surrounds = {
+    ["c"] = {
+      add = function()
+        local cmd = require("nvim-surround.config").get_input "Command: "
+        return { { "\\" .. cmd .. "{" }, { "}" } }
+      end,
+    },
+    ["e"] = {
+      add = function()
+        local env = require("nvim-surround.config").get_input "Environment: "
+        return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
+      end,
+    },
+  },
+}
