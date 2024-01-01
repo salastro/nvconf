8b0d446b73a569a86199bd750a221a9dd17a5908
salastro <salah2112004@gmail.com>
2024-01-01 17:28:39 +0200

    Ultisnip is 100 times better than Luasnip
    This reverts commit 5800f305a2921666ef8fd32444a241efe0e1edc0.

--- configs/cmp.lua
+++ configs/cmp.lua
@@ -62,8 +62,8 @@ local options = {
   },
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-      -- vim.fn["UltiSnips#Anon"](args.body)
+      -- require("luasnip").lsp_expand(args.body)
+      vim.fn["UltiSnips#Anon"](args.body)
     end,
   },
 
@@ -88,26 +88,26 @@ local options = {
       "c",
     }),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
+    -- ["<Tab>"] = cmp.mapping(function(fallback)
+    --   if require("luasnip").expand_or_jumpable() then
+    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+    --   else
+    --     fallback()
+    --   end
+    -- end, {
+    --   "i",
+    --   "s",
+    -- }),
+    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --   if require("luasnip").jumpable(-1) then
+    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+    --   else
+    --     fallback()
+    --   end
+    -- end, {
+    --   "i",
+    --   "s",
+    -- }),
   },
   sources = {
     { name = "nvim_lsp" },
--- plugins.lua
+++ plugins.lua
@@ -123,15 +123,10 @@ local plugins = {
   {
     "lervag/vimtex",
     ft = "tex",
-    dependencies = {
-      "iurimateus/luasnip-latex-snippets.nvim",
-    },
     config = function()
       require "custom.configs.vimtex"
       require("core.utils").load_mappings "vimtex"
-      require'luasnip-latex-snippets'.setup()
       -- or setup({ use_treesitter = true })
-      require("luasnip").config.setup { enable_autosnippets = true }
     end,
   },
 
@@ -175,12 +170,29 @@ local plugins = {
     end,
   },
 
-  -- -- To make a plugin not be loaded
-  -- -- LuaSnip
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   enabled = false,
-  -- },
+  -- To make a plugin not be loaded
+  -- LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    enabled = false,
+  },
+
+  -- UltiSnips
+  {
+    "SirVer/ultisnips",
+    dependencies = {
+      "honza/vim-snippets",
+      "quangnguyen30192/cmp-nvim-ultisnips",
+    },
+    event = "InsertEnter",
+    config = function()
+      vim.g.loaded_python3_provider = nil
+      vim.g.UltiSnipsExpandTrigger = "<Tab>"
+      vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
+      vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
+      vim.g.UltiSnipsEditSplit = "vertical"
+    end,
+  },
 
   {
     "tpope/vim-fugitive",
