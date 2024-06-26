8164b09346b747aa3f99baf4ced42a3883f4e0b9
salastro <salah2112004@gmail.com>
2024-06-27 02:43:43 +0300

    Add waka time

--- plugins.lua
+++ plugins.lua
@@ -182,6 +182,12 @@ local plugins = {
       mappings = true,
     },
   },
+
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
+
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
   -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
