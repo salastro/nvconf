cdd773b709b698a7fa55d3bd94b1465d21bdf818
salastro <salah2112004@gmail.com>
2024-09-06 22:01:46 +0300

    Remove format modified
    * Currently no formatting at all, plan to improve in the future.

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,23 +1,9 @@
-local on_attach = function(client, bufnr)
-  require("plugins.configs.lspconfig").on_attach(client, bufnr)
-
-  local augroup_id = vim.api.nvim_create_augroup("FormatModificationsDocumentFormattingGroup", { clear = false })
-  vim.api.nvim_clear_autocmds { group = augroup_id, buffer = bufnr }
-
-  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    group = augroup_id,
-    buffer = bufnr,
-    callback = function()
-      local lsp_format_modifications = require "lsp-format-modifications"
-      lsp_format_modifications.format_modifications(client, bufnr)
-    end,
-  })
-end
+local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "pyright", "texlab", "bashls", "zls" }
+local servers = { "html", "cssls", "tsserver", "pyright", "texlab", "bashls", "zls", "texlab" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
